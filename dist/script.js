0c8510efe44b3af3f78ffc37a22b387ff4758b27
Ish-Avanzado <isiaavanzado@gmail.com>
2025-04-11 15:48:38 +0800

    updating files

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,41 @@
-const burger = document.querySelector (".burger__menu");
-const hNav = document.querySelector (".header__action");
+const burger = document.querySelector(".burger__menu");
+const hNav = document.querySelector(".header__action");
 burger.addEventListener("click", () => {
-    burger.classList.toggle("open");
-    hNav.classList.toggle("open");
-})
+  burger.classList.toggle("open");
+  hNav.classList.toggle("open");
+});
+
+//slider
+
+const circleNav = document.querySelectorAll(".slider__nav");
+const circleContent = document.querySelectorAll(".slider__testimony");
+const circleDilaw = document.querySelectorAll(".slider__nav-circle");
+circleNav.forEach((circle) => {
+  circle.addEventListener("click", () => {
+    removeActiveCircle();
+    circle.classList.add("active");
+    const activeContent = document.querySelector(`#${circle.id}-content`);
+    removeActiveContent();
+    activeContent.classList.add("active");
+    const activeDilaw = document.querySelector(`#${circle.id}-dilaw`);
+    removeActiveDilaw();
+    activeDilaw.classList.add("active");
+  });
+});
+
+function removeActiveCircle() {
+  circleNav.forEach((circle) => {
+    circle.classList.remove("active");
+  });
+}
+
+function removeActiveContent() {
+  circleContent.forEach((content) => {
+    content.classList.remove("active");
+  });
+}
+function removeActiveDilaw() {
+  circleDilaw.forEach((dilaw) => {
+    dilaw.classList.remove("active");
+  });
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         homeBannerImage: "url(../dist/img/videoPoster.jpg)",
-        blackOverlay:"linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 100%)",
+        grayImage: "url(../dist/img/fca-bg-grayscale.jpg)",
+        scholarshipImage: "url(../dist/img/scholarship.webp)",
+        blackOverlay:
+          "linear-gradient(to bottom, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 100%)",
       },
       fontFamily: {
         raleway: ["Raleway", "sans-serif"],
@@ -17,7 +20,10 @@ module.exports = {
         darkblue: "#3b5897",
         lightblue: "#167ac6",
         green: "#28941e",
+        darkgreen: "#008542",
+        darkgray: "#4b5563",
         lightgray: "#d1d5db",
+        grayBG: "#f2f2f2",
       },
     },
   },
